5f7b965cd6c19a16b38d6a019f67c172eb45172a
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-05-01 18:16:35 -0400

    Completed Level 1 assignment, modified to arrow notation over function

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,10 @@
 let tableData = data;
 let tbody = d3.select("tbody");
 
-data.forEach(function(sightings) {
-    console.log(sightings);
+data.forEach((sightings) => {
     let row = tbody.append("tr");
-})
+    Object.entries(sightings).forEach(([key, value]) => {
+        let cell = row.append("td");
+        cell.text(value);
+    });
+});
