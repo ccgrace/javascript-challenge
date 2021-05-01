184daef88cd2987ba1fea4896825ae849caf5358
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-05-01 18:45:45 -0400

    added new lvl 2 file to include lvl 1 script

--- UFO-level-2/UFO lvl 2.js
+++ UFO-level-2/UFO lvl 2.js
@@ -1,2 +1,11 @@
 // from data.js
 let tableData = data;
+let tbody = d3.select("tbody");
+
+data.forEach((sightings) => {
+    let row = tbody.append("tr");
+    Object.entries(sightings).forEach(([key, value]) => {
+        let cell = row.append("td");
+        cell.text(value);
+    });
+});
