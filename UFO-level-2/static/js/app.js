680f381324262d66129d985069692d7f979ce7dc
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-05-02 17:11:43 -0400

    created unique lists for search menus

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,7 +1,14 @@
 // from data.js
 let tableData = data;
 let tbody = d3.select("tbody");
+let city = [...new Set(data.map(x => x.city))]
+let state = [...new Set(data.map(x => x.state))]
+let country = [...new Set(data.map(x => x.country))]
+let shape = [...new Set(data.map(x => x.shape))]
 
+// data.forEach(city.push(data.city))
+
+// Populates the table with all the data from the data file
 function populate_table(data){
     data.forEach((sightings) => {
         let row = tbody.append("tr");
@@ -11,4 +18,9 @@ function populate_table(data){
         });
     });
 };
-populate_table(data);
+
+
+function populate_dropdowns(table_data){
+    let options = d3.select("#city>option").attr("value", "").text(city)}
+
+populate_table(data)
